b28b67330b72b0749c1b6cdd840492d47b08afaf
bunthet01 <bunthetsay@gmail.com>
2019-10-11 18:49:33 +0200

    add hyperparameters_serching.py and update readme.md

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ROB311_TP4
-Train MNIST data set with SVM from Sklearn
+Train MNIST data set with SVM.SVC from Sklearn
 
 **Note** 
 
@@ -8,3 +8,72 @@ Download MNIST Dataset in zip file from this[link](https://www.kaggle.com/oddrat
 Extract the zip files containing the train and test dataset.
 
 In the current directory, create a folder ``data`` and put the two data sets in this folder. 
+## Train SVM
+To train the SVM by the default value of parameters, run:
+
+```
+
+python svm.py 
+```
+These are the parameters that you can try different value:
+
+```
+Argument to train SVM
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --c C                 penalty parameter of the error term
+  --k {rbf,linear,poly,sigmoid}
+                        Kernel type
+  --gamma {auto,scale}  Kernel coefficient for ‘rbf’, ‘poly’ and ‘sigmoid’
+  --train-size TRAIN_SIZE
+                        The training set size. '-1' mean that we use all the
+                        dataset
+  --seed SEED           The random seed.
+  --no-plot             Plot the confusion matrix
+  --vb                  Turn on verbose
+
+```
+
+
+## Hyperparameters search
+
+In SVM.SVC from Sklearn, there are three important hyperparameters:  `C`, `gamma`,`kernel`.
+Each hyperparameters belong to its list of possible values.
+
+We use `hyperparameters_search.py` to find which of the three parameters that give the best accuracy.
+Basically, we train svm for every possible combination of `C`, `gamma`,`kernel` of their list and we search for a set of {C, gamma, kernel} that maximize the accuracy.
+
+In terminal, run :
+```
+
+python hyperparameters_search.py
+
+```
+
+## Result
+
+The size of training data is very large, so in hyperparameter searching, we use only 3000 training expample,
+as a result, we find optimal hyperparameters:
+
+```
+C: 10
+kernel: "rbf"
+gamma: "scale"
+
+```
+with accuracy: `0.9692`.
+
+Finally, we train the whole data with:
+```
+python svm.py --k rbf --c 10 --gamma scale --vb 
+``` 
+#TODO
+We get the accuracy of: `0.`.
+The corresponding confusion matrix:
+
+
+
+
+
+
--- svm.py
+++ svm.py
@@ -9,9 +9,12 @@ import matplotlib.pyplot as plt
 parser = argparse.ArgumentParser(description="Argument to train SVM")
 
 parser.add_argument("--c", type=float, default=1.0, help="penalty parameter of the error term" )
-parser.add_argument("--k", type=str, default="rbf",  choices=["rbf","linear", "poly", "sigmoid"], help="Kernel type")
+parser.add_argument("--k", type=str, default="linear",  choices=["rbf","linear", "poly", "sigmoid"], help="Kernel type")
 parser.add_argument("--gamma", type=str, default="auto", choices=["auto","scale"], help="Kernel coefficient for ‘rbf’, ‘poly’ and ‘sigmoid’")
-parser.add_argument("--train-size", type=int, default=1000, help="The training set size")
+parser.add_argument("--train-size", type=int, default=-1, help="The training set size. '-1' mean that we use all the dataset ")
+parser.add_argument("--seed", type=int, default=1, help="The random seed.")
+parser.add_argument("--no-plot", default=False, action="store_true",help="Plot the confusion matrix")
+parser.add_argument("--vb",default=False,action="store_true",help="Turn on verbose")
 
 args = parser.parse_args()
 
@@ -20,16 +23,26 @@ args = parser.parse_args()
 train_data = pd.read_csv("data/mnist_train.csv").values
 test_data = pd.read_csv("data/mnist_test.csv").values
 
-if args.train_size>train_data.shape[0]:
+np.random.seed(args.seed)
+np.random.shuffle(train_data) 
+np.random.shuffle(test_data)
+
+
+if args.train_size>train_data.shape[0] or args.train_size ==-1:
     train_size = train_data.shape[0]
 else:
     train_size = args.train_size
+
+test_size = int(train_size/4)
+if test_size > test_data.shape[0]:
+    test_size = test_data.shape[0]
 
 # TODO: 
 # 1) write function to plot the image with the corespond label   
 # 2) If we use the train data in total, it takes alot of time. There are twos possible idea of improvement 
 #    2.1) Use HOG filter. See: https://scikit-image.org/docs/dev/auto_examples/features_detection/plot_hog.html
 #    2.2) Reduce the size of the image. See: https://scikit-image.org/docs/dev/auto_examples/transform/plot_rescale.html
+# 3) Write hyperparameter testing python file   
 
 
 def plot_confusion_matrix(confusion_matrix):
@@ -56,29 +69,31 @@ def plot_confusion_matrix(confusion_matrix):
 
 def train():
     # train 
-    classifer = SVC(C=args.c, kernel=args.k, gamma=args.gamma, verbose=True)
+    classifer = SVC(C=args.c, kernel=args.k, gamma=args.gamma, verbose=args.vb)
     start_training_time = time.time()
     classifer.fit(train_data[np.arange(train_size)][:,np.arange(1, train_data.shape[1])], train_data[np.arange(train_size)][:,0])
     print("\n================================")
     print('Training time: {0:.4f} seconds'.format(time.time()-start_training_time))
 
     # build confusion matrix
+    start_prediction_time = time.time()
     confusion_matrix = np.zeros((10,10))
-    for i in range(test_data.shape[0]):
+    for i in range(test_size):
         predicted_label = classifer.predict([test_data[i][np.arange(1, test_data.shape[1])]])
         for j in np.arange(10):
             if test_data[i][0] == j:
                 confusion_matrix[j][predicted_label]+=1
     print("Arguments used for training: ",args.__dict__)
-
+    print('Prediction time: {0:.4f} seconds'.format(time.time()-start_prediction_time))
     # Accuracy 
     acc = np.trace(confusion_matrix)/np.sum(confusion_matrix)
     print("Model Accuracy: {0:.4f} ".format(acc))
 
     # plot confusion matrix
-    plot_confusion_matrix(confusion_matrix)
-    
+    if not args.no_plot:
+        plot_confusion_matrix(confusion_matrix)
+
 
 if __name__ == "__main__":
-    train()
+    train() 
 
